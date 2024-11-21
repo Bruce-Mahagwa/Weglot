41b59ea3268c9cdb3d08fc795ed14dfe56af1da5
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-11-21 17:18:29 +0300

    Add the landing text component with functionality that pulls text upwards on the page when user presses the downward arrow key and does the opposite with the upward arrow key

--- weglotwebsitebuilder/src/Components/Landing.js
+++ weglotwebsitebuilder/src/Components/Landing.js
@@ -1,12 +1,14 @@
 // components
 import Navigation from "./Navigation";
 import ImagineLogo from "./ImagineLogo";
+import LandingText from "./LandingText";
 import ScrollBtn from "./ScrollBtn";
 const Landing = () => {
     return (
-        <div className = "w-full h-screen bg-violet-200 flex flex-col justify-between">
+        <div className = "w-full h-screen bg-violet-200 flex flex-col justify-between overflow-hidden relative">
             <Navigation />
             <ImagineLogo />
+            <LandingText />
             <ScrollBtn />
         </div>
     )
