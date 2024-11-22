d3a1cec2b8b90920a3d221020c977858bc4d52f3
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-11-22 18:36:46 +0300

    Harmonize the arrow clicks with scroll functionality for better ui experience

--- weglotwebsitebuilder/src/Components/Landing.js
+++ weglotwebsitebuilder/src/Components/Landing.js
@@ -4,10 +4,21 @@ import ImagineLogo from "./ImagineLogo";
 import LandingText from "./LandingText";
 import SideLandingText from "./SideLandingText";
 import ScrollBtn from "./ScrollBtn";
-const Landing = ({setLeaveFirstPage}) => {
+// hooks
+import { useEffect} from "react";
+const Landing = ({setLeaveFirstPage}) => {    
+    // ensure that onload we reset the page to the topmost position
+    useEffect(() => {
+        const handleOnLoad = () => {
+            window.scrollTo(0, 0);
+        }
+        window.addEventListener("beforeunload", handleOnLoad);
+        return () => window.removeEventListener("onbeforeunload", handleOnLoad);
+    }, [])
+
     return (
-        <div className = "relative bg-violet-200" style = {{height: "1100vh"}}>
-            <div className = "w-full h-screen flex flex-col justify-between overflow-hidden relative">
+        <div className = "relative bg-violet-200" style = {{height: "1000vh"}}>
+            <div className = "w-full h-screen flex flex-col justify-between overflow-hidden relative" id = "landing">
                 <Navigation />
                 <ImagineLogo />
                 <LandingText />
--- weglotwebsitebuilder/src/Components/hooks/useDetectDownArrow.js
+++ weglotwebsitebuilder/src/Components/hooks/useDetectDownArrow.js
@@ -1,26 +1,47 @@
 // dependencies
 import {useState, useEffect} from "react";
 
-export const useDetectDownArrow = () => {
+export const useDetectDownArrow = (refs=null) => {
     // counts how many times a user has clicked the down arrow or page down arrow or how many times a user has clicked the up arrow or the page up key
     const [countDownArrowClicked, setCountDownArrowClicked] = useState(0);
+    const [scrollDistance, setScrollDistance] = useState(0); // detects how far a user has scrolled
+    const step = window.innerHeight / 10; // maximum number of clicks a user has when on the landing page
+
     useEffect(() => {
         const handleKeyDown = (e) => {
             if (e.key === "ArrowUp" || e.key === "PageUp") {
                 setCountDownArrowClicked((prev) => prev - 1);
+                // scroll by distance
                 if (countDownArrowClicked < 1) {
                     setCountDownArrowClicked(0);
+                }
+                if (e.key === "ArrowUp") {
+                    window.scrollBy(0, -100)
                 }                
+                console.log(countDownArrowClicked, window.innerHeight, -countDownArrowClicked * step)
             }
             else if (e.key === "ArrowDown" || e.key === "PageDown") {
                 setCountDownArrowClicked((prev) => prev + 1);
-                if (countDownArrowClicked > 12) {
-                    setCountDownArrowClicked(12)
+                if (countDownArrowClicked >= 12) {
+                    setCountDownArrowClicked(11)
                 }
+                if (e.key === "ArrowDown") {
+                    window.scrollBy(0, 100)
+                }
+                console.log(countDownArrowClicked, window.innerHeight, window.scrollHeight,  step)
             }            
         }
+
+        const handleScroll = () => {
+            const scrollTop = window.scrollY || (document.documentElement || document.body.parentNode || document.body).scrollTop
+            setScrollDistance(scrollTop)
+        }
         window.addEventListener("keydown", handleKeyDown);
-        return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [countDownArrowClicked])
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, [countDownArrowClicked, scrollDistance])
     return countDownArrowClicked;
 }
